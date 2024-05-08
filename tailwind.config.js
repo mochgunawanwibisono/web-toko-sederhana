4697ae6629ab895aed99001b5d0005ae0e177551
Gunawan IT <gunawan.itdepartemen@gmail.com>
2024-05-08 09:07:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html','cara-belanja.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#4f46e5',
+      }
+    },
+  },
+  plugins: [],
+}
+
